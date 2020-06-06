66af4af5adec9e11e5b07f59e5291700dff95852
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-05 19:13:29 -0500

    Se creó la funcionalidad de crear ads. Se terminó la clase: 'Implementación de plugin de Ads'.

--- assets/index.ts
+++ assets/index.ts
@@ -1,23 +1,28 @@
-import MediaPlayer from './MediaPlayer'
-import AutoPlay from './plugins/AutoPlay'
-import AutoPause from './plugins/AutoPause'
+import MediaPlayer from './MediaPlayer';
+import AutoPlay from './plugins/AutoPlay';
+import AutoPause from './plugins/AutoPause';
+import Ads from './plugins/Ads';
 
-const video = document.querySelector("video")
-const player = new MediaPlayer(
-    {
-        el: video,
-        plugins: [
-            new AutoPlay(),
-            new AutoPause()
-        ]
-    })
+const video = document.querySelector('video');
+const player = new MediaPlayer({
+  el: video,
+  plugins: [new AutoPlay(), new AutoPause(), new Ads()],
+});
 
-const button:HTMLElement = document.querySelector("button")
+const playButton: HTMLElement = document.querySelector('#playButton');
+playButton.onclick = () => player.togglePlay();
 
-button.onclick = () => player.play()
+const muteButton: HTMLElement = document.querySelector('#muteButton');
+muteButton.onclick = () => {
+  if (player.media.muted) {
+    player.unmute();
+  } else {
+    player.mute();
+  }
+};
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js').catch(error => {
-        console.log(error.message);
-    })
+  navigator.serviceWorker.register('/sw.js').catch(error => {
+    console.log(error.message);
+  });
 }
